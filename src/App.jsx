1bb17613d238855df5f984f1e44284fe070e62aa
Carlos-marinho <carloslucas.tavares@gmail.com>
2024-08-08 15:34:57 -0300

    Add Search and Input to consume weather informations API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
+import axios from 'axios'
+import WeatherInformations from './components/WeatherInformations'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef()
+  const API_KEY = "9b4f3141d26f73b08f6c33588bd73989"
+  const [weather,setWeather] = useState()
+  const [weather5days,setWeather5days] = useState()
+
+  async function handleSearch() {
+    try {
+      const city = inputRef.current.value
+      const api = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`)
+      const api5days = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`)
+      setWeather(api.data)
+      setWeather5days(api5days.data)
+        
+    } catch (error) {
+      console.log(error)
+    }
+  }           
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className='w-screen h-screen bg-blue-400'>
+      <div className="flex flex-col w-8/12 mx-auto justify-center items-center gap-y-6">
+        <h1 className='text-5xl font-extrabold text-white my-10'>Weather Forecast</h1>
+        <input className='shadow-md px-2 py-1 rounded-lg outline-none' ref={inputRef} type="text" placeholder='Enter a city name' />
+        <button className='text-lg px-4 py-2 bg-blue-950 hover:bg-blue-100 hover:text-blue-950 rounded-xl text-white' onClick={handleSearch}>Search</button>
+      </div>
+    
+      {weather && <WeatherInformations weather={weather}/>}
+      
+    </div>
   )
 }
 
